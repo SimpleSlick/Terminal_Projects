79bc3fcd8528d549faf6b01a7b530c61f7bb5d6e
SimpleSlick <b40881067@gmail.com>
2026-07-06 22:31:06 +0530

    Initial Commit

---
+++ Kingdom_Arcade/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("=".repeat(40));
+        System.out.println(" ".repeat(8) + "Welcome to Kingdom Arcade");
+        System.out.println("=".repeat(40));
+
+        scan.close();
+    }
+}
